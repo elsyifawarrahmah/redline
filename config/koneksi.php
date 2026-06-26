a44cd73ad10c20f12a53632f1eb6bf85c146bcb1
elsyifawarrahmah <syifawarohma1@gmail.com>
2026-06-26 19:09:44 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,14 +6,17 @@ $pass = getenv('MYSQLPASSWORD') ?: 'dGxYaGBGEMmKOlfauVaPqsFdzneeDkNP';
 $port = getenv('MYSQLPORT') ?: '3306';
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        PDO::MYSQL_ATTR_SSL_CA => false,
-    ];
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            PDO::MYSQL_ATTR_SSL_CA => '',
+        ]
+    );
     $conn = $pdo;
 } catch (PDOException $e) {
     die("DB Error: " . $e->getMessage());
