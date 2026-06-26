cb707068a16ccc09300965308e00a3a4ccceba9d
elsyifawarrahmah <syifawarohma1@gmail.com>
2026-06-26 19:14:59 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,21 +2,14 @@
 $host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $dbname = getenv('MYSQLDATABASE') ?: 'railway';
 $user = getenv('MYSQLUSER') ?: 'root';
-$pass = getenv('MYSQLPASSWORD') ?: 'dGxYaGBGEMmKOlfauVaPqsFdzneeDkNP';
+$pass = getenv('MYSQLPASSWORD') ?: 'UAoiJuPVOSPIXhNqKVUHjVSDftAupCHM';
 $port = getenv('MYSQLPORT') ?: '3306';
 
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            PDO::MYSQL_ATTR_SSL_CA => '',
-        ]
-    );
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
     $conn = $pdo;
 } catch (PDOException $e) {
     die("DB Error: " . $e->getMessage());
